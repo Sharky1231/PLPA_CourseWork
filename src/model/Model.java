15f5b44c151176d0e41cbd866e44ae2b5d489289
Sharky <sharky1231@gmail.com>
2016-04-15 18:05:23 +0200

    General UI and custom canvas

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,7 +1,4 @@
 package model;
 
-/**
- * Created by Sharky on 14/04/2016.
- */
 public class Model {
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,7 +1,172 @@
 package view;
 
-/**
- * Created by Sharky on 14/04/2016.
- */
-public class View {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.JFrame;
+
+public class View extends JFrame{
+
+    private CustomCanvas canvasArea;
+    private javax.swing.JTextArea codeTextArea;
+    private javax.swing.JTextArea errorTextArea;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JButton submitButton;
+    private javax.swing.JTextField textField;
+
+    public View() {
+        init();
+        addExtraSettings();
+        registerEvents();
+    }
+
+    private void addExtraSettings() {
+        textField.grabFocus();
+        codeTextArea.setEditable(false);
+        errorTextArea.setEditable(false);
+
+        setSize(1000, 700);
+        setResizable(true);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getRootPane().setDefaultButton(submitButton);
+        setLocationRelativeTo(null);
+    }
+
+    private void init() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        submitButton = new javax.swing.JButton();
+        textField = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        codeTextArea = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        errorTextArea = new javax.swing.JTextArea();
+        canvasArea = new CustomCanvas();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        submitButton.setText("Submit");
+
+        codeTextArea.setColumns(20);
+        codeTextArea.setLineWrap(true);
+        codeTextArea.setRows(5);
+        jScrollPane2.setViewportView(codeTextArea);
+
+        jLabel2.setText("Commands");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+                jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 285, Short.MAX_VALUE)
+                                        .addComponent(submitButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(textField)
+                                        .addGroup(jPanel2Layout.createSequentialGroup()
+                                                .addComponent(jLabel2)
+                                                .addGap(0, 0, Short.MAX_VALUE)))
+                                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+                jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(textField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(submitButton)
+                                .addGap(1, 1, 1)
+                                .addComponent(jLabel2)
+                                .addGap(3, 3, 3)
+                                .addComponent(jScrollPane2)
+                                .addContainerGap())
+        );
+
+        errorTextArea.setColumns(20);
+        errorTextArea.setLineWrap(true);
+        errorTextArea.setRows(5);
+        jScrollPane3.setViewportView(errorTextArea);
+
+        jLabel1.setText("Errors");
+
+        jLabel3.setText("Canvas");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                .addComponent(jScrollPane3)
+                                                .addComponent(canvasArea, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE))
+                                        .addComponent(jLabel1)
+                                        .addComponent(jLabel3))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(canvasArea, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>
+
+    private class ButtonHandler implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == submitButton) {
+                if (!textField.getText().isEmpty()) {
+                    codeTextArea.append(textField.getText() + "\n");
+                    textField.setText("");
+                    canvasArea.drawTest();
+                }
+            }
+        }
+    }
+
+    private void registerEvents() {
+        ActionListener listener = new ButtonHandler();
+        submitButton.addActionListener(listener);
+    }
+
+    public void setError(String error) {
+        errorTextArea.append(error + "\n");
+    }
+
+
+    public void showView() {
+        setVisible(true);
+    }
 }
